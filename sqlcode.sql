3ca5afa28303a298f03463aa3857adf4adbc8a3c
ritik <ritiksharma0723@gmail.com>
2022-08-16 09:26:21 +0530

    commit

--- sqlcode.sql
+++ sqlcode.sql
@@ -1,15 +1,31 @@
-create database exam;
 
-create table student(
-    roll int AUTO_INCREMENT UNIQUE,
-    fname varchar(20), 
-    lname varchar(20),
-    phone varchar(10) UNIQUE,
-    email varchar(60) UNIQUE,
-    semester int,
-    uid char(20) PRIMARY KEY);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-INSERT into student (roll,fname,lname,phone,email,semester,uid) values (
-    1,'Ritik','Sharma','8866892314','ritikss748@gmail.com',3,'21004500210167');
-INSERT into student (fname,lname,phone,email,semester,uid) values (
-   'Shanu','Pandey','9106692865','shanukumarpandey2003@gmail.com',3,'21004500210088');   
+
+CREATE TABLE `student` (
+  `roll` int(11) NOT NULL,
+  `fname` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lname` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phone` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `semester` int(11) DEFAULT NULL,
+  `uid` char(20) COLLATE utf8_unicode_ci NOT NULL,
+  `pass` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `student` (`roll`, `fname`, `lname`, `phone`, `email`, `semester`, `uid`, `pass`) VALUES
+(2, 'Shanu', 'Pandey', '9106692865', 'shanukumarpandey2003@gmail.com', 3, '21004500210088', 'shanu123'),
+(1, 'Ritik', 'Sharma', '8866892314', 'ritikss748@gmail.com', 3, '21004500210167', 'ritik123');
+
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`uid`),
+  ADD UNIQUE KEY `roll` (`roll`),
+  ADD UNIQUE KEY `phone` (`phone`),
+  ADD UNIQUE KEY `email` (`email`);
+
+ALTER TABLE `student`
+  MODIFY `roll` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
