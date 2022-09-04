b11fac9e11400ee306f2a1eaa995548b438e60aa
RitikSharma23 <100440065+RitikSharma23@users.noreply.github.com>
2022-09-04 16:37:50 +0530

    commit

---
+++ js/ADMIN/admin.js
@@ -0,0 +1,8 @@
+
+fname=document.getElementById("fname").innerHTML;
+lname=document.getElementById("lname").innerHTML;
+email=document.getElementById("email").innerHTML;
+phone=document.getElementById("phone").innerHTML;
+details="&fname="+fname+"&lname="+lname+"&email="+email+"&phone="+phone;  
+
+document.getElementById("n_name").innerHTML=fname+" "+lname
